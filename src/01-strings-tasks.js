87a06bd297abb84b3fb543380ba086871b3922d7
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2020-12-14 15:54:58 +0300

    feat: implement 65

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -134,8 +134,9 @@ function repeatString(value, count) {
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const regex = new RegExp(value);
+  return str.replace(regex, '');
 }
 
 /**
