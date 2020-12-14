a5570a8ffd5cae9a64c6102f836aeda2245560ac
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2020-12-14 23:58:04 +0300

    feat: implement 75

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -174,8 +174,14 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  // eslint-disable-next-line func-names
+  const fun = function () {
+    count += 1;
+    return count - 1;
+  };
+  return fun;
 }
 
 
