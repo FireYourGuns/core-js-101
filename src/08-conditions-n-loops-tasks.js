412ee425e0fd3447af3440a2c4f65e71de59ae88
FireYourGuns <60598547+FireYourGuns@users.noreply.github.com>
2020-12-14 14:40:17 +0300

    feat: implement 64

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -344,8 +344,18 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const arr = num.toString().split('');
+  let sum = 0;
+  arr.forEach((element) => {
+    sum += +element;
+  });
+  const newArr = sum.toString().split('');
+  let newSum = 0;
+  newArr.forEach((element) => {
+    newSum += +element;
+  });
+  return newSum;
 }
 
 
